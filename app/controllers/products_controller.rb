62eac6e0d046a65dfb5f26bcd7c1b65d3aa5be17
yogiduzit <yverma2@my.bcit.ca>
2019-07-03 14:42:41 -0700

    added products controller files

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,26 @@
+class ProductsController < ApplicationController
+
+  def new
+    @product = Product.new
+  end
+
+  def index
+
+    @products = Product.all
+  end
+
+  def create
+    product = params.require(:product).permit(:title, :description, :price)
+    if product["price"].to_f != 0.0
+      product["price"] = product["price"].to_f
+    end
+    
+    @product = Product.new product
+
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+end
