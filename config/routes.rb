1a267075da59afc565a1754610374b2bd8b40738
yogiduzit <yverma2@my.bcit.ca>
2019-07-03 14:42:17 -0700

    Added routes for creating and getting record

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # Index page
   get('/', {to: 'welcome#index', as: 'root'});
+
+  # About page
   get('/about', {to: 'welcome#about'});
 
+  # Contacts Page
   get('/contact', {to: 'contacts#index'});
 
+  # New product page
+  get '/products/new', {to: 'products#new'}
+
+  # All products page
+  get '/products', {to: 'products#index'}
+
+  # Create new contact
   post('/contact', {to: 'contacts#create'});
   
+  # Create new product
+  post '/products', {to: 'products#create'}
 end
