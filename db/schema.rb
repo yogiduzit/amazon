eefef1f6367f774c6d0f3a96c5c7dbbfae2c077c
yogiduzit <yverma2@my.bcit.ca>
2019-07-04 13:31:25 -0700

    Created review model/table referencing product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_02_222833) do
+ActiveRecord::Schema.define(version: 2019_07_04_202931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2019_07_02_222833) do
     t.integer "hit_count"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "body"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -33,4 +42,5 @@ ActiveRecord::Schema.define(version: 2019_07_02_222833) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reviews", "products"
 end
