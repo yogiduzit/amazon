1e46bcfa1c544cae171cf56180699d213277cd5d
yogiduzit <yverma2@my.bcit.ca>
2019-07-04 15:21:59 -0700

    Added foreign key constraint, create and destroy

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationController
+  def create
+    @review = Review.new review_params
+    @product = Product.find(params["product_id"])
+
+    @review.product = @product
+    
+
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render 'products/show'
+    end
+  end
+
+  def destroy
+
+    @review = Review.find(params["id"])
+    @review.destroy
+
+    redirect_to product_path(@review.product)
+  end
+  private
+  def review_params
+    params.require('review').permit(:body, :rating)
+  end
+end
