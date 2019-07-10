fb2c8a945cfe2c74277b207155eaff8b6d9c1d0b
yogiduzit <yverma2@my.bcit.ca>
2019-07-10 14:21:15 -0700

    Added the create newsArticle tests

--- app/controllers/news_articles_controller.rb
+++ app/controllers/news_articles_controller.rb
@@ -2,4 +2,21 @@ class NewsArticlesController < ApplicationController
   def new
     @news_article = NewsArticle.new
   end
+
+  def create
+    @news_article = NewsArticle.new news_article_params
+
+    if @news_article.valid?
+      @news_article.save
+      redirect_to news_articles_path(@news_article)
+    else
+      render :new
+    end
+
+  end
+
+  private
+  def news_article_params
+    params.require(:news_article).permit(:title, :description, :like_count)
+  end
 end
--- spec/controllers/news_articles_controller_spec.rb
+++ spec/controllers/news_articles_controller_spec.rb
@@ -16,4 +16,61 @@ RSpec.describe NewsArticlesController, type: :controller do
       expect(response).to render_template(:new)
     end
   end
+
+  describe "#create" do
+    context "valid data" do
+
+      it "must redirect to the show page" do
+        post(:create, params: {
+          news_article: {
+            title: "Yogiduzit",
+            description: "nfjkdnfsmfnsdmfndsm,f sfsdf s  fnsfnds,fnsdf sfbsjkdfnsd fs fksdbf sdfsdhkfbmsd qekw w"
+          }
+        })
+
+        expect(response).to redirect_to(news_articles_path(NewsArticle.last))
+      end
+
+      it "must create a new entry in the db" do
+        before_create = NewsArticle.count
+
+        post(:create, params: {
+          news_article: {
+            title: "Yogi",
+            description: "nfjkdnfsmfnsdmfndsm,f sfsdf s  fnsfnds,fnsdf sfbsjkdfnsd fs fksdbf sdfsdhkfbmsd qekw w"
+          }
+        })
+        after_create = NewsArticle.count
+
+        expect(after_create).to eq(before_create + 1)
+      end
+    end
+
+    context "invalid data" do
+      it "must render the new page" do 
+        post(:create, params: {
+          news_article: {
+            title: nil,
+            description: "nfjkdnfsmfnsdmfndsm,f sfsdf s  fnsfnds,fnsdf sfbsjkdfnsd fs fksdbf sdfsdhkfbmsd qekw w"
+          }
+        })
+
+        expect(response).to(render_template(:new))
+      end
+
+      it "must not add an entry to the db" do
+        before_create = NewsArticle.count
+
+        post(:create, params: {
+          news_article: {
+            title: nil,
+            description: "nfjkdnfsmfnsdmfndsm,f sfsdf s  fnsfnds,fnsdf sfbsjkdfnsd fs fksdbf sdfsdhkfbmsd qekw w"
+          }
+        })
+        after_create = NewsArticle.count
+
+        expect(after_create).to eq(before_create)
+      end
+    end
+  end
 end
