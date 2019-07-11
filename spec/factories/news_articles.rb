aaa064a63ecfda4bbbb82a8aae88070111fe997f
yogiduzit <yverma2@my.bcit.ca>
2019-07-11 14:31:40 -0700

    Associated news article factory with user

--- spec/factories/news_articles.rb
+++ spec/factories/news_articles.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :news_article do
     title {Faker::Job.title}
-    description {"fskdjfnsdfsdfjskfnsdv dsknslfksd, dsvdsflnsdwqwdwnkjfewjknfwfnejkwfnwkfnewjknfewjkfnwkjefnwejkfnjknkj"}
+    description {"fskdjfnsdfsdfjskfnsdv dsknslfksd, dsvdsflnsdwqwdwnkjfewjknfwfnejkwfnwkfnewjknfewjkf"}
+    association(:user, factory: :user)
   end
 end
