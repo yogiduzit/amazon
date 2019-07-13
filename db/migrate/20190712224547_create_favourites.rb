66fe209414b125611a4b845d6cf55fd4ea33cd60
yogiduzit <yverma2@my.bcit.ca>
2019-07-12 18:38:45 -0700

    Added favourites model and migration

---
+++ db/migrate/20190712224547_create_favourites.rb
@@ -0,0 +1,9 @@
+class CreateFavourites < ActiveRecord::Migration[5.2]
+  def change
+    create_table :favourites do |t|
+      t.references :product, foreign_key: true
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_12_203729) do
+ActiveRecord::Schema.define(version: 2019_07_12_224547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favourites", force: :cascade do |t|
+    t.bigint "product_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_favourites_on_product_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.bigint "user_id"
@@ -67,6 +76,8 @@ ActiveRecord::Schema.define(version: 2019_07_12_203729) do
     t.string "password_digest"
   end
 
+  add_foreign_key "favourites", "products"
+  add_foreign_key "favourites", "users"
   add_foreign_key "likes", "reviews"
   add_foreign_key "likes", "users"
   add_foreign_key "news_articles", "users"
