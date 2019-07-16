cee572686ecfd982b1b1b04af15194c92a8681f0
yogiduzit <yverma2@my.bcit.ca>
2019-07-16 14:51:48 -0700

    Created logic for mail

---
+++ app/mailers/product_mailer.rb
@@ -0,0 +1,21 @@
+class ProductMailer < ApplicationMailer
+  def hello_world
+    mail(
+      to: "itsyog35h@gmail.com",
+      from: "info@codecore.ca",
+      subject: "Hello World!"
+    )
+  end
+
+  def new_product(product)
+
+    @product = product
+    @product_owner = @product.user
+
+    mail(
+      to: @product_owner.email,
+      subject: "You created a product named #{@product.title}"
+    )
+
+  end
+end
